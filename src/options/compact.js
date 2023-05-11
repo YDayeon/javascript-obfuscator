7ed88654ae9ed5971981a1a681807544e24dbf5a
YDayeon <88532969+YDayeon@users.noreply.github.com>
2023-05-11 10:54:12 +0900

    compact

--- src/options/compact.js
+++ src/options/compact.js
@@ -8,3 +8,6 @@ console.log(`The result is ${result}`);
 
 // Obfuscated Code with compact option enabled:
 // function multiply(a,b){return a*b}let result=multiply(2,5);console.log(`The result is ${result}`);
+
+// 예시 코드 설명
+// 모든 불필요한 공백과 주석을 제거하여 더 간결하고 읽기 어려운 코드를 만듭니다. 그러나 코드의 기능은 여전히 동일하며, 난독화 된 코드의 출력은 원래 코드와 동일합니다.
